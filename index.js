c63abd47065cf3dc55593b53fe40dd717f31fb1e
Shane Amal <21stwebwizard@gmail.com>
2023-06-28 00:06:14 +0530

    V 1.0 done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// javascript
+const menu = document.querySelector(".menu")
+      selectOptions = document.querySelector(".select_options")
+      optionsStyle = window.getComputedStyle(selectOptions)
+      hearts = document.querySelectorAll(".far")
+
+
+menu.addEventListener("click", ()=> {
+    if(optionsStyle.getPropertyValue("clip-path") === "circle(0% at 100% 0%)") {
+        selectOptions.style.clipPath = "circle(100% at 50% 50%)"
+    }else {
+        selectOptions.style.clipPath = "circle(0% at 100% 0%)"
+    }
+})
+
+document.addEventListener('mouseup', (event)=> {
+    if(optionsStyle.getPropertyValue('clip-path') === "circle(100% at 50% 50%)"){
+        if(!selectOptions.contains(event.target)){
+            selectOptions.style.clipPath = "circle(0% at 100% 0%)"  
+        }
+    }
+})
+
+function changeHeart(x) {
+    x.classList.toggle("fas")
+     x.classList.toggle("far")
+}
